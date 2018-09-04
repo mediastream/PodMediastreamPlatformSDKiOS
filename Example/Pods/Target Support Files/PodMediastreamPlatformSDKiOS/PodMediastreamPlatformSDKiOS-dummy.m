b6ca3c955797bdd37b575048068b006afe68a984
cruiz <ruizcarlos1985@gmail.com>
2018-09-04 18:46:13 -0300

    Create bare project via pod

---
+++ Example/Pods/Target Support Files/PodMediastreamPlatformSDKiOS/PodMediastreamPlatformSDKiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodMediastreamPlatformSDKiOS : NSObject
+@end
+@implementation PodsDummy_PodMediastreamPlatformSDKiOS
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodMediastreamPlatformSDKiOS_Example/Pods-PodMediastreamPlatformSDKiOS_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodMediastreamPlatformSDKiOS_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PodMediastreamPlatformSDKiOS_Example
+@end
